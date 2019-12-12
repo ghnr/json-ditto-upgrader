ca2e568000a43dad0cd3aa132279f41dac609473
ghnr <45798027+ghnr@users.noreply.github.com>
2019-12-12 13:18:18 +0000

    Require lodash each directly

--- test/index.js
+++ test/index.js
@@ -2,28 +2,31 @@
 
 const assert = require('assert');
 const ditto_v2 = require('json-ditto-v2');
-const _ = require('lodash');
+const { each } = require('lodash');
 
 let dittoUpgrader = require('../index')
 let data = require('./data');
 
 describe('ditto upgrader', () => {
-  _.each(data, function(serviceObj, testService) {
-    let sample = serviceObj.sample;
-    let oldMapping = serviceObj.oldMapping;
-    let upgradedMapping = dittoUpgrader.dittoUpgrader(oldMapping);
-    let expectedResult = serviceObj.expectedResult;
+  describe('upgraded results should be identical to the expected results', () => {
+    each(data, function(serviceObj, testService) {
+      let sample = serviceObj.sample;
+      let oldMapping = serviceObj.oldMapping;
+      let upgradedMapping = dittoUpgrader.dittoUpgrader(oldMapping);
+      let expectedResult = serviceObj.expectedResult;
 
-    new ditto_v2(upgradedMapping).unify(sample).then((result) => {
-      for (let key in expectedResult) {
-        if (key === "id" || key === "createdAt") {
-          continue;
-        }
-        it(`${testService}: should have the same ${key} value as the expected result`, () => {
-          assert.deepStrictEqual(result[key], expectedResult[key])
-        });  
-      }    
+      new ditto_v2(upgradedMapping).unify(sample).then((result) => {
+        for (let key in expectedResult) {
+          if (key === "id" || key === "createdAt") {
+            continue;
+          }
+          it(`${testService}: should have the same ${key} value as the expected result`, () => {
+            assert.deepStrictEqual(result[key], expectedResult[key])
+          });  
+        }    
+      });
     });
+    // Dummy to make mocha it() work inside of each()
+    it('', function () {});
   });
-  it('', function () {});
 });
